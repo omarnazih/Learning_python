a2a352d1ff950386643cba84c01ede2dc4b2648a
omar nazih <omarnazeh295@gmail.com>
2020-08-19 15:17:53 +0200

    scraping tech with tim

--- web scraping/app.py
+++ web scraping/app.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 #chrome webdriver path
 PATH = "C:\Program Files (x86)\chromedriver.exe"     
-
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://9xbuddy.org/")
+driver.get("https://techwithtim.net/")
+print(driver.title)
+
+
+search = driver.find_element_by_name("s")
+search.send_keys("Test")
+search.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(5)
+	
 driver.close()
